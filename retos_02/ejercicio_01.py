c1135ecae8b34401a6cb3c49b25402b24866e9b1
LeoFavio <leofavio.vs@gmail.com>
2025-09-03 00:49:37 -0400

    Añadido el ejercicio_01.py

---
+++ retos_02/ejercicio_01.py
@@ -0,0 +1,14 @@
+num1 = int(input(""))
+num2 = int(input(""))
+
+suma=num1+num2
+print("Suma:",suma)
+
+resta=num1-num2
+print("Resta:",resta)
+
+multiplicacion=num1*num2
+print("Multiplicación:",multiplicacion)
+
+division=num1/num2
+print("División:",division)
